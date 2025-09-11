d2a4ff50266bd32593c406c5f90ab6c0697b50d2
Khushi-1803 <khushi011504@gmail.com>
2025-09-11 10:33:41 +0530

    fixed

--- quizApp/src/App.jsx
+++ quizApp/src/App.jsx
@@ -1,18 +1,16 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import WelcomePage from "./pages/WelcomePage.jsx";
 import Category from "./pages/Category.jsx";
 import QuizPage from "./pages/QuizPage.jsx";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<WelcomePage />} />
-        <Route path="/category" element={<Category />} />
-        <Route path="/quiz" element={<QuizPage />} />
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/" element={<WelcomePage />} />
+      <Route path="/category" element={<Category />} />
+      <Route path="/quiz" element={<QuizPage />} />
+    </Routes>
   );
 }
 
